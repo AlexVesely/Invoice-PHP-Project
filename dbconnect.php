eab5ca5587e48892588da2ea6eec81227dc11255
AlexVesely <veselyalex11@gmail.com>
2024-12-20 14:04:43 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 // This file should normally be put in a folder with certain security access right
-// remember to change the username and password below
+// remember to change the username and password below if necessary
 $conn = mysqli_connect("localhost", "root", "");
 if(!$conn) {
     die ("Error connecting to MySQL: " . mysqli_error($conn));
@@ -10,7 +10,5 @@ $db_select_success =  mysqli_select_db($conn, "cw2");
 
 if(!$db_select_success) {
     die ("Error selecting database: ".mysqli_error($conn));
-} else {
-	// echo "MySQL database: cdcol selected. <br/>";
 }
 ?>
