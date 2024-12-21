c6ed39ed387b10f7478768f0aab811deb93e49b3
AlexVesely <veselyalex11@gmail.com>
2024-12-21 09:51:23 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,13 @@
 <?php
 // This file should normally be put in a folder with certain security access right
-// remember to change the username and password below if necessary
+// Remember to change the username and password below if necessary
 $conn = mysqli_connect("localhost", "root", "");
 if(!$conn) {
     die ("Error connecting to MySQL: " . mysqli_error($conn));
 }
 
-$db_select_success =  mysqli_select_db($conn, "cw2");
+// Change "invoiceDatabase" to the name of your created database
+$db_select_success =  mysqli_select_db($conn, "invoiceDatabase");
 
 if(!$db_select_success) {
     die ("Error selecting database: ".mysqli_error($conn));
